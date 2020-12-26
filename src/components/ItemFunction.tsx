3ad05ac3f2bce842407b9be299fe85599c590fed
quang8122000 <bestxd2k@gmail.com>
2020-12-26 14:44:46 +0700

    update new Home Student

--- src/components/ItemFunction.tsx
+++ src/components/ItemFunction.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import {View, StyleSheet, Text, Pressable} from 'react-native';
+import {View, StyleSheet, Text, Pressable, Image} from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import LinearGradient from 'react-native-linear-gradient';
+import shadow from '../utils/shadow';
 
 export const ItemFunction = (props: any) => {
+  const {color, source, title} = props;
+
   return (
     <Pressable style={styles.MainContainer}>
-      <LinearGradient
-        colors={['#4c669f', '#3b5998', '#192f6a']}
-        style={styles.abc}>
-        <Text style={[styles.title]}>Xem lịch</Text>
+      <LinearGradient colors={color} style={styles.abc}>
+        <Image source={source} style={styles.Icon} />
+        <Text style={[styles.title]}>{title}</Text>
       </LinearGradient>
     </Pressable>
   );
@@ -25,10 +27,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   title: {
+    fontSize: wp('4.5'),
     alignSelf: 'center',
+    fontFamily: 'roboto-slab-bold',
+    color: '#FFFFFF',
+    marginTop: hp('2'),
   },
   abc: {
-    height: hp('20'),
+    height: hp('25'),
     width: wp('40'),
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: wp('2'),
+    ...shadow(6),
+  },
+  Icon: {
+    height: hp('6'),
+    width: wp('12'),
   },
 });
--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-import {Login, SplashScreen} from '../screens';
-import {ItemFunction} from '../components';
+import {Login, SplashScreen, HomeStudent} from '../screens';
+
 import {from} from 'rxjs';
 const Stack = createStackNavigator();
 const mainRoute = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator headerMode={'none'}>
-        <Stack.Screen name={'ItemFuction'} component={ItemFunction} />
-
         <Stack.Screen name={'Login'} component={Login} />
+        {/* <Stack.Screen name={'HomeStudent'} component={HomeStudent} /> */}
         <Stack.Screen name={'SplashScreen'} component={SplashScreen} />
       </Stack.Navigator>
     </NavigationContainer>
